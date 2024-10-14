723646c31e7acd9c5944a7366fcee47a6af366b3
foxpilot64 <jedioutcastbex@gmail.com>
2024-10-14 13:46:59 -0400

    fixed movement so W moves fwd correctly and D moves backwards.

--- player.py
+++ player.py
@@ -7,15 +7,15 @@ from circleshape import CircleShape
 class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
-        self.rotation = 180
+        self.rotation = 0
 
    
     def triangle(self):
-        forward = pygame.Vector2(0, 1).rotate(self.rotation)
-        right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
+        forward = pygame.Vector2(0, -1).rotate(self.rotation)
+        right = forward.rotate(90)
         a = self.position + forward * self.radius
-        b = self.position - forward * self.radius - right
-        c = self.position - forward * self.radius + right
+        b = self.position - forward * self.radius - right * self.radius / 1.5
+        c = self.position - forward * self.radius + right * self.radius / 1.5
         return [a, b, c]
 
     # Overriding the draw method from the CircleShape base class:
@@ -23,7 +23,7 @@ class Player(CircleShape):
         pygame.draw.polygon(screen, "white", self.triangle(), 2)
 
     def rotate(self, dt):
-        self.rotation += PLAYER_TURN_SPEED * dt
+        self.rotation -= PLAYER_TURN_SPEED * dt
 
     def update(self, dt):
         # Check if mvt and rotation keys are pressed
@@ -31,10 +31,10 @@ class Player(CircleShape):
         
         # Handle fwd and back mvt
         if keys[pygame.K_w]:
-            self.move(-dt) 
+            self.move(dt) 
         
         if keys[pygame.K_s]:
-            self.move(dt) 
+            self.move(-dt) 
 
         # Handle rotation
         if keys[pygame.K_a]:
@@ -45,6 +45,6 @@ class Player(CircleShape):
     def move(self, dt):
         # Starts with a vector facing up from (0,1) which represents the default fwd direction.
         # Then rotate the vector by the player's rotation attribute.
-        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        forward = pygame.Vector2(0, -1).rotate(-self.rotation)
         #Scale the vector to account for speed and time.
         self.position += forward * PLAYER_SPEED * dt
